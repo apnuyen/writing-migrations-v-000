cea5ae423f8dcaa0000f18cb0eefff4bc5577481
An Nuyen <annuyen@gmail.com>
2018-02-23 20:39:48 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
-  def change 
-    create_table 
-  end 
-  
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+
 end
